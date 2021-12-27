8f5cc6f645ed507b23c8a2c33cf65b1b4858c694
vandananayak <nvandana1@gmail.com>
2021-12-27 16:17:22 +0530

    servercomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NewComponent } from './new/new.component';
+
+import { ServerComponent } from './server/server.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, NewComponent],
-  bootstrap: [AppComponent, NewComponent],
+  declarations: [AppComponent, ServerComponent],
+  bootstrap: [AppComponent, ServerComponent],
 })
 export class AppModule {}
